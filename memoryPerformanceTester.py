20aa04b1e97d64851b29e208787c69ab30b1a253
LsFbr <louisfabre@hotmail.fr>
2025-05-18 14:51:23 +0200

    fix(memoryPerformanceTester.py, timePerformancesTester.py): update for using with last version of parse_csv() function wich should take "optimize" argument at False when parsing csv for bruteforce algorithme.

--- memoryPerformanceTester.py
+++ memoryPerformanceTester.py
@@ -14,31 +14,37 @@ def measure_memory(func, actions, max_cost):
 
 def main():
     max_cost = 500
-    action_counts = []
+    action_not_optimized_counts = []
+    action_optimized_counts = []
     bruteforce_memory = []
     optimized_memory = []
 
     # Charger les actions depuis le CSV
-    actions = parse_csv(csv_path, optimize=True)
+    actions_not_optimized = parse_csv(csv_path, optimize=False)
+    actions_optimized = parse_csv(csv_path, optimize=True)
 
     # Tester pour différentes tailles de listes d'actions
-    for i in range(1, len(actions)+1):
-        subset_actions = actions[:i]
-        action_counts.append(i)
+    for i in range(1, len(actions_not_optimized)+1):
+        subset_actions = actions_not_optimized[:i]
+        action_not_optimized_counts.append(i)
 
         # Mesurer l'utilisation de la mémoire pour bruteforce
         bruteforce_mem = measure_memory(bruteforce, subset_actions, max_cost)
         bruteforce_memory.append(bruteforce_mem)
+
+    for i in range(1, len(actions_optimized)+1):
+        subset_actions = actions_optimized[:i]
+        action_optimized_counts.append(i)
 
         # Mesurer l'utilisation de la mémoire pour knapsack
         optimized_mem = measure_memory(knapsack, subset_actions, max_cost)
         optimized_memory.append(optimized_mem)
 
     # Tracer les résultats
-    plt.figure(figsize=(10, 6))
-    plt.plot(action_counts, bruteforce_memory, label='Bruteforce', marker='o')
-    plt.plot(action_counts, optimized_memory, label='Optimized', marker='x')
-    plt.xticks(range(0, len(actions) + 1))
+    plt.figure(figsize=(7, 7))
+    plt.plot(action_not_optimized_counts, bruteforce_memory, label='Bruteforce', marker='o')
+    plt.plot(action_optimized_counts, optimized_memory, label='Optimized', marker='x')
+    plt.xticks(range(0, len(actions_not_optimized) + 1))
     plt.yticks(range(0, int(max(bruteforce_memory) + 10), 10))
     plt.xlabel('Nombre d\'actions')
     plt.ylabel('Utilisation de la mémoire (MiB)')
--- timePerformancesTester.py
+++ timePerformancesTester.py
@@ -16,31 +16,36 @@ def measure_time(func, actions, max_cost):
 
 def main():
     max_cost = 500
-    action_counts = []
+    action_not_optimized_counts = []
+    action_optimized_counts = []
     bruteforce_times = []
     optimized_times = []
 
     # Charger les actions depuis le CSV
-    actions = parse_csv(csv_path, optimize=True)
+    actions_optimized = parse_csv(csv_path, optimize=True)
+    actions_not_optimized = parse_csv(csv_path, optimize=False)
 
     # Tester pour différentes tailles de listes d'actions
-    for i in range(1, len(actions) + 1):
-        subset_actions = actions[:i]
-        action_counts.append(i)
+    for i in range(1, len(actions_not_optimized) + 1):
+        subset_actions = actions_not_optimized[:i]
+        action_not_optimized_counts.append(i)
 
         # Mesurer le temps pour bruteforce
         bruteforce_time = measure_time(bruteforce, subset_actions, max_cost)
         bruteforce_times.append(bruteforce_time)
 
+    for i in range(1, len(actions_optimized) + 1):
+        subset_actions = actions_optimized[:i]
+        action_optimized_counts.append(i)
         # Mesurer le temps pour knapsack
         optimized_time = measure_time(knapsack, subset_actions, max_cost)
         optimized_times.append(optimized_time)
 
     # Tracer les résultats
-    plt.figure(figsize=(10, 6))
-    plt.plot(action_counts, bruteforce_times, label='Bruteforce', marker='o')
-    plt.plot(action_counts, optimized_times, label='Optimized', marker='x')
-    plt.xticks(range(0, len(actions) + 1))
+    plt.figure(figsize=(7, 7))
+    plt.plot(action_not_optimized_counts, bruteforce_times, label='Bruteforce', marker='o')
+    plt.plot(action_optimized_counts, optimized_times, label='Optimized', marker='x')
+    plt.xticks(range(0, len(actions_not_optimized) + 1))
     plt.xlabel('Nombre d\'actions')
     plt.ylabel('Temps d\'exécution (secondes)')
     plt.title('Comparaison de la complexité temporelle')
